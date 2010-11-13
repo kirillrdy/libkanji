f751337118a74bc9e65b16a7358e89845564a63e
Robert Gravina <robert@gravina.com>
2010-11-13 10:46:24 +0800

    spec refactoring

--- spec/libkanji/sentence_spec.rb
+++ spec/libkanji/sentence_spec.rb
@@ -4,14 +4,12 @@ gem 'rspec-rails'
 
 module LibKanji
   describe Sentence do
-    describe "parsing" do
-      it "should break a valid sentence into words" do
-        text = IO.read("data/ruby.txt")
-        text = "取って\n名付けられた。"
+    it "should break a valid sentence into words" do
+      text = IO.read("data/ruby.txt")
+      text = "取って\n名付けられた。"
 
-        words = Sentence.parse(text).map{|x|x.word}
-        words.should == ["取って", "\n", "名", "付け", "ら", "れ", "た", "。"]
-      end
+      words = Sentence.parse(text).map{|x|x.word}
+      words.should == ["取って", "\n", "名", "付け", "ら", "れ", "た", "。"]
     end
   end
 end
