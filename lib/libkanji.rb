36ea394d55f0a0bc01536c3481f16a250ada89de
Kirill Radzikhovskyy <kirillvr@kirillvr-TOSHIBA-NB305.(none)>
2010-11-13 13:35:20 +1100

    added Conjugations

--- lib/libkanji.rb
+++ lib/libkanji.rb
@@ -1,4 +1,5 @@
 require_relative 'libkanji/dictionary'
 require_relative 'libkanji/sentence'
+require_relative 'libkanji/conjugations'
 require_relative 'libkanji/dictionary_word'
 require_relative 'libkanji/sentence_word'
--- lib/libkanji/dictionary_word.rb
+++ lib/libkanji/dictionary_word.rb
@@ -2,7 +2,7 @@
 class LibKanji::DictionaryWord
   attr_accessor :word
   attr_accessor :pronunciation
-  attr_accessor :type
+  attr_accessor :types
   attr_accessor :meaning
   
   def to_s
@@ -11,47 +11,18 @@ class LibKanji::DictionaryWord
     "#{stuff}: #{@type.join ","} #{@meaning}"
   end
   
-  def initialize(word,pronunciation,type,meaning)
+  def initialize(word,pronunciation,types,meaning)
     @word = word
     @pronunciation = pronunciation
-    @type = type
+    @types = types
     @meaning = meaning
   end
   
   def conjugations
     list = []
-    if type.include? "v1"
-      [ "ない",'ます','ましょう','たい','なさい','られる','れば','よう','た','て'].each {|x| list << self.word.gsub(/る$/,x)}
-    end
-    if type.include? "v5u"
-      ['わない','います','いましょう','いたい','いなさい','える','え','えば','おう','った','って'].each {|x| list << self.word.gsub(/う$/,x)}
-    end
-    if type.include? "v5k"
-      ['かない','きます','きましょう','きたい','きなさい','ける','け','けば','こう','いた','いて'].each {|x| list << self.word.gsub(/く$/,x)}
-    end
-    if type.include? "v5s"
-      ['さない','します','しましょう','したい','しなさい','せる','せ','せば','そう','した','して'].each {|x| list << self.word.gsub(/す$/,x)}
-    end
-    if type.include? "v5t"
-      ['たない','ちます','ちましょう','ちたい','ちなさい','てる','て','てば','とう','った','って'].each {|x| list << self.word.gsub(/つ$/,x)}
-    end
-    if type.include? "v5n"
-      ['なない','にます','にましょう','にたい','になさい','ねる','ね','ねば','のう','んだ','んで'].each {|x| list << self.word.gsub(/ぬ$/,x)}
-    end
-    if type.include? "v5m"
-      ['まない','みます','みましょう','みたい','みなさい','める','め','めば','もう','んだ','んで'].each {|x| list << self.word.gsub(/む$/,x)}
-    end
-    if type.include? "v5r"
-      ['らない','ります','りましょう','りたい','りなさい','れる','れ','れば','ろう','った','って'].each {|x| list << self.word.gsub(/る$/,x)}
-    end
-    if type.include? "v5g"
-      ['がない','ぎます','ぎましょう','ぎたい','ぎなさい','げる','げ','げば','ごう','いだ','いで'].each {|x| list << self.word.gsub(/ぐ$/,x)}
-    end
-    if type.include? "v5b"
-      ['ばない','びます','びましょう','びたい','びなさい','べる','べ','べば','ぼう','んだ','んで'].each {|x| list << self.word.gsub(/ぶ$/,x)}
-    end
-    if type.include? 'adj-na'
-      list << (self.word + 'な')
+    for type in @types
+      list_of_conjugations =  Conjugations.new(@word,type).conjugations
+      list += list_of_conjugations if list_of_conjugations
     end
     return list
   end
