a2c71a7f19353c774d45b30d0acc56b99d71a829
Robert Gravina <robert@gravina.com>
2010-11-13 12:44:21 +0800

    Refactored Sentence parse method

--- lib/libkanji/sentence.rb
+++ lib/libkanji/sentence.rb
@@ -10,27 +10,14 @@ module LibKanji
     #
     def self.parse(sentence)
       list_of_words_in_sentence = []
-
       start = 0
 
       while start < sentence.length
-
-        @words_from_dictionary = nil
-        @matched_part_from_text = nil
-
-        for finish in (start..sentence.length-1)
-          lookup sentence[start..finish]
-        end
-
-        start += (@matched_part_from_text.length - 1) if @words_from_dictionary
-
-        key_to_add = @matched_part_from_text || sentence[start..start]
-        
-        list_of_words_in_sentence << SentenceWord.new(key_to_add, @words_from_dictionary)
-
-        start += 1
+        start, next_word = next_word(sentence, start)
+        list_of_words_in_sentence << next_word
       end
-      return list_of_words_in_sentence
+      
+      list_of_words_in_sentence
     end
     
     private
@@ -42,5 +29,23 @@ module LibKanji
       end      
     end
 
+    def self.next_start_pointer(start)
+      @words_from_dictionary ? start + (@matched_part_from_text.length - 1) : start
+    end
+
+    def self.next_word(sentence, start)
+      @words_from_dictionary = nil
+      @matched_part_from_text = nil
+      
+      for finish in (start..sentence.length-1)
+        lookup sentence[start..finish]
+      end
+      
+      start = next_start_pointer(start)      
+      word_in_sentence = @matched_part_from_text || sentence[start..start]
+
+      start += 1
+      [start, SentenceWord.new(word_in_sentence, @words_from_dictionary)]
+    end
   end
 end
