b2cac61d893a279a2fc9fc48ddcf42a7908561a2
Robert Gravina <robert@gravina.com>
2010-11-13 21:13:11 +0800

    Better noun support. Pasted EDICT docs into model as it was a pain to keep looking them up.

--- lib/libkanji/conjugations.rb
+++ lib/libkanji/conjugations.rb
@@ -1,4 +1,64 @@
 # encoding: utf-8
+#
+# Part of Speech Marking (from EDICT docs)
+# 
+# The following POS markings are currently used:
+# 
+#   adj-i adjective (keiyoushi)
+#   adj-na  adjectival nouns or quasi-adjectives (keiyodoshi)
+#   adj-no  nouns which may take the genitive case particle `no'
+#   adj-pn  pre-noun adjectival (rentaishi)
+#   adj-t `taru' adjective
+#   adj-f noun or verb acting prenominally (other than the above)
+#   adj former adjective classification (being removed)
+#   adv adverb (fukushi)
+#   adv-n adverbial noun
+#   adv-to  adverb taking the `to' particle
+#   aux auxiliary
+#   aux-v auxiliary verb
+#   aux-adj auxiliary adjective
+#   conj  conjunction
+#   ctr counter
+#   exp Expressions (phrases, clauses, etc.)
+#   id  idiomatic expression
+#   int interjection (kandoushi)
+#   iv  irregular verb
+#   n noun (common) (futsuumeishi)
+#   n-adv adverbial noun (fukushitekimeishi)
+#   n-pref  noun, used as a prefix
+#   n-suf noun, used as a suffix
+#   n-t noun (temporal) (jisoumeishi)
+#   num numeric
+#   pn  pronoun
+#   pref  prefix
+#   prt particle
+#   suf suffix
+#   v1  Ichidan verb
+#   v5  Godan verb (not completely classified)
+#   v5aru Godan verb - -aru special class
+#   v5b Godan verb with `bu' ending
+#   v5g Godan verb with `gu' ending
+#   v5k Godan verb with `ku' ending
+#   v5k-s Godan verb - iku/yuku special class
+#   v5m Godan verb with `mu' ending
+#   v5n Godan verb with `nu' ending
+#   v5r Godan verb with `ru' ending
+#   v5r-i Godan verb with `ru' ending (irregular verb)
+#   v5s Godan verb with `su' ending
+#   v5t Godan verb with `tsu' ending
+#   v5u Godan verb with `u' ending
+#   v5u-s Godan verb with `u' ending (special class)
+#   v5uru Godan verb - uru old class verb (old form of Eru)
+#   v5z Godan verb with `zu' ending
+#   vz  Ichidan verb - zuru verb - (alternative form of -jiru verbs)
+#   vi  intransitive verb
+#   vk  kuru verb - special class
+#   vn  irregular nu verb
+#   vs  noun or participle which takes the aux. verb suru
+#   vs-i  suru verb - irregular
+#   vs-s  suru verb - special class
+#   vt  transitive verb
+#
 class Conjugations
   def initialize(word,type)
     @word = word
@@ -42,13 +102,16 @@ class Conjugations
         ['く','くない','くて','かった','ければ','くなかった'].map {|x| @word.gsub(/い$/,x)}
       when 'adj-na'
         [(@word + 'な')]
-      when 'adj-t', 'adj-f', 'adj-no', 'adj-pn'
+      when 'adj-pn'
+        # as far as I know, nothing should be done to these rentaishi
+        [@word]
+      when 'adj-t', 'adj-f', 'adj-no'
         puts "'#{@type}' is currently unsupported"
       #
       # Nouns
       #
       when "n"
-        ['だ'].map {|x| @word + x}
+        ['だ', 'の', 'で', 'でない', 'だった', 'なら'].map {|x| @word + x}
     end
   end
 end
