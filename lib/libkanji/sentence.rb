9c35f8d4e89141b8b80e7ab6a079a548c91fbdaa
Robert Gravina <robert@gravina.com>
2010-11-13 11:44:09 +0800

    refactoring

--- lib/libkanji/sentence.rb
+++ lib/libkanji/sentence.rb
@@ -15,29 +15,34 @@ module LibKanji
 
       while start < sentence.length
 
-        words_from_dictionary = nil
-        matched_part_from_text = nil
+        @words_from_dictionary = nil
+        @matched_part_from_text = nil
 
         for finish in (start..sentence.length-1)
-          part = sentence[start..finish]
-          if Dictionary.has_word? part
-            words_from_dictionary = Dictionary.search(part)
-            matched_part_from_text = part
-          end
+          lookup sentence[start..finish]
         end
 
-        if words_from_dictionary
-          start += matched_part_from_text.length - 1
+        if @words_from_dictionary
+          start += @matched_part_from_text.length - 1
         end
 
-        key_to_add = matched_part_from_text || sentence[start..start]
+        key_to_add = @matched_part_from_text || sentence[start..start]
         
-        list_of_words_in_sentence << SentenceWord.new(key_to_add,words_from_dictionary)
+        list_of_words_in_sentence << SentenceWord.new(key_to_add, @words_from_dictionary)
 
         start += 1
       end
       return list_of_words_in_sentence
     end
     
+    private
+    
+    def self.lookup(part)
+      if Dictionary.has_word? part
+        @words_from_dictionary = Dictionary.search(part)
+        @matched_part_from_text = part
+      end      
+    end
+
   end
 end
