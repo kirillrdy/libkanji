78802204d1426e5cbac44bc67bfcf2ca4c1b49dd
Robert Gravina <robert@gravina.com>
2010-11-13 11:33:00 +0800

    basic specs for dictionary

--- spec/libkanji/dictionary_spec.rb
+++ spec/libkanji/dictionary_spec.rb
@@ -4,5 +4,12 @@ gem 'rspec-rails'
 
 module LibKanji
   describe Dictionary do
+    it "should allow searching for a word" do
+      Dictionary.search("来る").should be_true
+    end
+
+    it "should tell you if a word is in the dictionary or not" do
+      Dictionary.has_word?("来る").should be_true
+    end
   end
 end
