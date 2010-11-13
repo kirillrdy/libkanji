51faed75cd65c34b937a350f1c8021d433513cb5
Robert Gravina <robert@gravina.com>
2010-11-13 10:27:03 +0800

    Created Gemfile with rspec and friends. Basic rspec port of test_libkanji.

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source :gemcutter
+
+# testing 
+group :test do
+  gem "factory_girl", :require => nil
+  gem "rspec", "2.1.0", :require => nil
+  gem "rspec-rails", "2.1.0", :require => nil
+  gem "rspec_scenarios", :require => nil
+end
---
+++ spec/libkanji/sentence_spec.rb
@@ -0,0 +1,18 @@
+# encoding: utf-8
+require_relative "../../lib/libkanji"
+gem 'rspec-rails'
+
+module LibKanji
+  describe Sentence do
+    describe "parsing" do
+      it "should break a valid sentence into words" do
+        text = IO.read("data/ruby.txt")
+        text = "取って\n名付けられた。"
+
+        words = Sentence.parse(text).map{|x|x.word}
+        words.should == ["取って", "\n", "名", "付け", "ら", "れ", "た", "。"]
+      end
+    end
+  end
+end
+
