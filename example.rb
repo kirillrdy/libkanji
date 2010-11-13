710d10ac58dd2f4c085333410805f40b5c57b1bb
Robert Gravina <robert@gravina.com>
2010-11-13 20:45:13 +0800

    Added extra spec in for nouns. Don't forget to regenerate /temp/dic_dump

--- example.rb
+++ example.rb
@@ -12,10 +12,9 @@ text = IO.read("data/ruby.txt")
 text = "床に野菜くずが散らかっていて、スリッパの裏が真っ黒になるくらい汚いそこは、異様に広いといい。 
 ひと冬軽く越せるような食料が並ぶ巨大な冷蔵庫がそびえ立ち、その銀の扉に私はもたれかかる。 
 油が飛び散ったガス台や、さびのついた包丁からふと目を上げると、窓の外には淋しく星が光る。 "
-
 puts Dictionary.search "散らかって"
 
-
+text = "私がこの世でいちばん好きな場所は台所だと思う。"
 puts Sentence.parse(text).map{|x|x.word }.inspect
 
 #text.lines.each do|line|
--- spec/libkanji/sentence_spec.rb
+++ spec/libkanji/sentence_spec.rb
@@ -4,12 +4,20 @@ gem 'rspec-rails'
 
 module LibKanji
   describe Sentence do
-    it "should break a valid sentence into words" do
+    it "should break a a simple valid sentence into words" do
       text = IO.read("data/ruby.txt")
       text = "取って\n名付けられた。"
 
       words = Sentence.parse(text).map{|x|x.word}
       words.should == ["取って", "\n", "名", "付け", "ら", "れ", "た", "。"]
+    end
+  
+    it "should break a complex sentence into words" do    
+      text = "私がこの世でいちばん好きな場所は台所だと思う。"
+      words = Sentence.parse(text).map{|x|x.word}
+
+      # いちばん should be a single word, but for simplicity we don't look up words by pronunciation 
+      words.should == ["私", "が", "この世", "で", "い", "ち", "ば", "ん", "好きな", "場所", "は", "台所だ", "と", "思う", "。"]
     end
   end
 end
