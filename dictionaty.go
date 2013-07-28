75a264f552c1446a1155af20fe0d69ca82db2dd3
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2013-07-28 13:14:03 +1000

    Cleanined up go dictionary parsing a bit, still lots to do

--- dictionaty.go
+++ dictionaty.go
@@ -6,16 +6,25 @@ import "fmt"
 
 
 func main() {
-	dictionary_words, err := regexp.Compile(`(.*?) \[(.*?)\] (.*?)\n`)
+	dictionary_entry_regex, err := regexp.Compile(`(.*?) \[(.*?)\] (.*?)\n`)
 	if err != nil { panic(err) }
 
 	text, err := ioutil.ReadFile("edict2_utf8")
-	list_of_strings := dictionary_words.FindAllStringSubmatch(string(text),-1)
-	for _, word := range list_of_strings {
+	if err != nil { panic(err) }
+
+	dictionary_entries := dictionary_entry_regex.FindAllStringSubmatch(string(text),-1)
+	for _, dictionary_entry := range dictionary_entries {
 		fmt.Println("************ Start of word ****************")
-		for _, sub_march := range word {
-			fmt.Printf("'%s' \n", sub_march)
-		}
+
+		words := dictionary_entry[1]
+		fmt.Printf("'%s' \n", words)
+
+		pronunciations := dictionary_entry[2]
+		fmt.Printf("'%s' \n", pronunciations)
+
+		meanings := dictionary_entry[3]
+		fmt.Printf("'%s' \n", meanings)
+
 		fmt.Println("End of word \n")
 	}
 }
