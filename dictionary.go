14270adddb998edc908a17c17f02755599b83ed5
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2013-10-06 12:15:53 +1100

    Added function for generating dictionary

--- dictionary.go
+++ dictionary.go
@@ -1,34 +1,42 @@
-package main
+package libkanji
 import "regexp"
 import "io/ioutil"
-import "fmt"
+//import "fmt"
 import "strings"
-import "./zfsdb"
 
-func main() {
+type DictionaryWord struct {
+	Word string
+	Pronunciation string
+	Meanings []string
+}
+
+type Dictionary []DictionaryWord
+
+func LoadDictionary() Dictionary {
+
+	var dictionary Dictionary
+
 	dictionary_entry_regex, err := regexp.Compile(`(.*?) \[(.*?)\] (.*?)\n`)
 	if err != nil { panic(err) }
 
 	// TODO dont load all at once
-	text, err := ioutil.ReadFile("edict2_utf8")
+	text, err := ioutil.ReadFile("libkanji/edict2_utf8")
 	if err != nil { panic(err) }
 
 	dictionary_entries := dictionary_entry_regex.FindAllStringSubmatch(string(text),-1)
-	for _, dictionary_entry := range dictionary_entries {
-		fmt.Println("************ Start of word ****************")
+
+	//TODO remove slicing
+	for _, dictionary_entry := range dictionary_entries[0:1000] {
 
 		words := dictionary_entry[1]
 		for _, word  := range strings.Split(words, ";") {
 
 			//TODO strip '()' from words
-			dictionary_word := zfsdb.CreateObject()
-			dictionary_word.WriteFact("type", "dictionary_word")
-			dictionary_word.WriteFact("word",word)
 
-			dictionary_word.WriteFact("pronunciation", dictionary_entry[2])
-
-			dictionary_word.WriteFact("meaning", dictionary_entry[3])
-
+			dictionary_word := DictionaryWord{Word: word, Pronunciation: dictionary_entry[1], Meanings: strings.Split(dictionary_entry[3],",")}
+			dictionary = append(dictionary, dictionary_word)
 		}
 	}
+	return dictionary
 }
+
