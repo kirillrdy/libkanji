913b75d0ee8876e7f1fe69442cb260c3bfb88683
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2013-10-11 08:59:50 +1100

    fixed up get_dict for mac

--- get_dict.sh
+++ get_dict.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 set -e
 wget -c http://ftp.monash.edu.au/pub/nihongo/edict2.gz
-zcat edict2.gz > edict2
-iconv -feuc-jp -tutf-8 edict2 > edict2_utf8
+gunzip edict2.gz
+iconv -f euc-jp -t utf-8 edict2 > edict2_utf8
 rm edict2
-rm edict2.gz
