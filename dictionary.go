385a478f48f795e43ac4cb78999bed75fbc73b2a
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2015-10-31 14:47:18 +1100

    Split LoadDictionary into smaller functions to isolate io

--- dictionary.go
+++ dictionary.go
@@ -2,32 +2,43 @@ package libkanji
 
 import (
 	"bufio"
+	"io"
 	"os"
 	"path"
 	"runtime"
 )
 
-type DictionaryLookupMap map[string][]DictionaryEntry
+type dictionaryLookupMap map[string][]DictionaryEntry
 
+//Dictionary main type that represents a dictionary
 type Dictionary struct {
-	bigHash DictionaryLookupMap
+	bigHash dictionaryLookupMap
 }
 
-func LoadDictionary() Dictionary {
-
-	var dictionary Dictionary
-	dictionary.bigHash = make(DictionaryLookupMap)
-
+//Edict2File returns ReaderCloser of a bundled edict2 file
+func Edict2File() io.ReadCloser {
 	_, currentSourceFile, _, _ := runtime.Caller(0)
 	path := path.Join(path.Dir(currentSourceFile), "edict2_utf8")
 	edictFile, err := os.Open(path)
-	defer edictFile.Close()
-
 	if err != nil {
 		panic(err)
 	}
+	return edictFile
+}
 
-	edictFileScanner := bufio.NewScanner(edictFile)
+//LoadDictionary parses edict2 file and performs conjugations of all words
+func LoadDictionary() Dictionary {
+	edictFile := Edict2File()
+	defer edictFile.Close()
+	return ParseDictionary(edictFile)
+}
+
+//ParseDictionary takes a reader and parses its input as a Dictionary
+func ParseDictionary(reader io.Reader) Dictionary {
+	var dictionary Dictionary
+	dictionary.bigHash = make(dictionaryLookupMap)
+
+	edictFileScanner := bufio.NewScanner(reader)
 
 	for edictFileScanner.Scan() {
 		word := ParseDictionaryLine(edictFileScanner.Text())
@@ -38,8 +49,6 @@ func LoadDictionary() Dictionary {
 				dictionary.bigHash[conjugations[i]] = append(dictionary.bigHash[conjugations[i]], word)
 			}
 		}
-
 	}
-
 	return dictionary
 }
